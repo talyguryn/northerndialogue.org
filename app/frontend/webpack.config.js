0403e89a86198784f8b554ad95bcaf3fa95f0457
Taly <vitalik7tv@yandex.ru>
2019-01-03 16:58:57 +0300

    Add slim framework

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -5,14 +5,14 @@ module.exports = {
   /**
    * Define entry point
    */
-  entry: path.resolve(__dirname, 'public', 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'frontend', 'src', 'index.js'),
 
   /**
    * Set bundle params
    */
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public', 'dist'),
+    path: path.resolve(__dirname, 'frontend', 'dist'),
     library: 'app',
   },
 
